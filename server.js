6182d279976b22d82185b20a3b17e11f6369aaad
Samuel Pai <samuelpai16@gmail.com>
2021-04-25 17:30:11 -0700

    set up the file structure and installed dependencies for this project

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+const exphbs = require("express-handlebars");
+
+// Initialize the app and create a port
+const app = express();
+const PORT = process.env.PORT || 5030;
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Set up body parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// Start the server on the port
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
