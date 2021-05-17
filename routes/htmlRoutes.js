9490853bed17471f60441b4403913bac873b3ec8
OSUMed <62990411+OSUMed@users.noreply.github.com>
2021-05-17 06:03:56 -0700

    Update htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -24,8 +24,25 @@ router.get("/orders", (req, res) => {
     res.render("orders");
 })
 
+// router.get("/paymentMethods", (req, res) => {
+//     res.render("payment_methods");
+// })
+
 router.get("/paymentMethods", (req, res) => {
-    res.render("payment_methods");
+  let sqlQuery = "SELECT * FROM Payment_Methods";
+  mysql.pool.query(sqlQuery, (err, result) => {
+      if (err) {
+              console.log(err);
+          }
+      else {
+          // handlebars accepts an object an then one property that should hold an array of values.
+          let data = {
+              paymentMethod: result
+          }
+          res.render("payment_methods", data);
+      }
+
+      })
 })
 
 router.get("/products", (req, res) => {
